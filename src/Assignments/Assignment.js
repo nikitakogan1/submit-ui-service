50b697b2321b2d16f70990b34a74b360026048a1
Nikita <nikita.kogan@sap.com>
2021-07-27 17:19:33 +0300

    fix file upload func

--- src/Assignments/Assignment.js
+++ src/Assignments/Assignment.js
@@ -1,11 +1,13 @@
 import { Component } from "react";
 import {getLoggedInUserName} from "../Utils/session";
+import Form from "react-bootstrap/Form"
+import "./Assignment.css"
 
 export default class Assignment extends Component{
     constructor(props){
         super(props)
         this.state = {
-            info: null
+            info: null, selectedFiles: null
         }
     }
     componentDidMount(){
@@ -36,13 +38,56 @@ export default class Assignment extends Component{
         });
     }
 
+    onChangeHandler=event=>{
+        var files = event.target.files;
+        console.log("the files",files);
+        console.log(this.validateSize(files));
+        if(this.validateSize(files)){ 
+            console.log("files",files);
+        // if return true allow to setState
+            this.setState({selectedFiles: files});
+        } else {
+            // files are too big.
+            //TODO: pop up
+        }
+}
 
+    fileUploadHandler = () => {
+        const data = new FormData()
+        console.log(this.state.selectedFile);
+        data.append('file', this.state.selectedFile)
+        console.log("Data",data);
+        //send the request
+        //TODO: Ask david about how to make the requests.
+ 
+  };
 
+    validateSize=(files)=>{
+        let sum = 0
+        let size = 30000;
+        console.log("filessss", JSON.stringify(files))
+        for (let i=0;i<files.length;i++){
+            console.log(files[i].size);
+            sum += files[i].size
+            if (sum > size) {
+                return false
+            }
+        }
+        return true
+    };
 
 
     render(){
         return (
-            <div> Hello from assignment {window.location.pathname}</div>
+            //should remove hello later.
+            <div> Hello from assignment {window.location.pathname}
+            <Form onSubmit={this.fileUploadHandler}>
+                <Form.Group onChange={this.onChangeHandler} controlId="formFileMultiple" className="mb-3">
+                    <Form.Label >Submit assignment</Form.Label>
+                    <Form.Control id="submitAssignmentBut" type="file" multiple  size="lg" />
+                </Form.Group> 
+            </Form>
+            </div>
         )
     }
 
