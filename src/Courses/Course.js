4d09465523fd26620cfb6c8b06baeeef39ef178c
david.abramov <david.abramov@servicenow.com>
2021-08-01 22:17:52 +0300

    course form

--- src/Courses/Course.js
+++ src/Courses/Course.js
@@ -107,10 +107,10 @@ export default class Course extends Component {
                     </Col>
                 </Form.Row>
                 <Modal open={this.state.showAssignmentsModal} center onClose={() => {this.setState({showAssignmentsModal: false})}}>
-                    <CourseAssignmentDefs courseNumber={this.state.number.toString()} courseYear={this.state.year.toString()}/>
+                    <div style={{"max-height": 600, "max-width": 600}}><CourseAssignmentDefs history={this.props.history} courseNumber={this.state.number.toString()} courseYear={this.state.year.toString()}/><br></br></div>
                 </Modal>
                 <Modal open={this.state.showAppealsModal} center onClose={() => {this.setState({showAppealsModal: false})}}>
-                    Appeals Modal
+                    <div style={{"max-height": 600, "max-width": 600}}><CourseAppeals history={this.props.history} courseNumber={this.state.number.toString()} courseYear={this.state.year.toString()}/><br></br></div>
                 </Modal>
             </Fragment>
         )
@@ -119,6 +119,7 @@ export default class Course extends Component {
 }
 
 const AlertNoAssignments = () => <div class="alert alert-info" role="alert">No Assignments Yet...</div>;
+const AlertNoAppeals = () => <div class="alert alert-info" role="alert">No Appeals Yet...</div>;
 
 class CourseAssignmentDefs extends Component {
 
@@ -129,14 +130,19 @@ class CourseAssignmentDefs extends Component {
             left_to_process: false,
             limit: 5,
             after_id: 0,
-            elements: null
+            elements: null,
+            assignmentToDelete: null,
+            showNewAssignmentModal: false
         }
         this.courseNumber = props.courseNumber;
         this.courseYear = props.courseYear;
+        this.isLoaded = this.isLoaded.bind(this);
         this.componentDidMount = this.componentDidMount.bind(this);
         this.previousPage = this.previousPage.bind(this);
         this.nextPage = this.nextPage.bind(this);
         this.goToBackEnd = this.goToBackEnd.bind(this);
+        this.deleteAssignment = this.deleteAssignment.bind(this);
+        this.createAssignment = this.createAssignment.bind(this);
     }
 
     isLoaded() {
@@ -175,13 +181,63 @@ class CourseAssignmentDefs extends Component {
           }
           return response.json()
         })
-        .then (data => {
+        .then(data => {
+          if (data.elements !== null) {
+            for (let i = 0; i < data.elements.length; i++) {
+            data.elements[i]["key"] = data.elements[i]["course"] + ":" + data.elements[i]["name"];
+            } 
+          }
           this.setState({elements:data.elements}, () => this.setState({isLoaded: true}));
         });
     }
 
     componentDidMount() {
         this.goToBackEnd();
+    }
+
+    onAssignmentSelectedForDeletion = (row, isSelect, rowIndex, e) => {
+        this.setState({assignmentToDelete: row.name});
+    }
+
+    deleteAssignment() {
+        fetch(window.location.origin + "/api/assignment_definitions/" + this.courseNumber + "/" + this.courseYear + "/" + this.state.assignmentToDelete,
+        {method: "DELETE"}).then((resp) => {
+            if (resp.status !== 200) {
+                alert("error deleting assignment definition. Status code is " + resp.status);
+            } else {
+                alert("assignment defintion deleted successfully");
+            }
+            this.props.history.go(0);
+        }).catch(err => alert(err));
+    }
+
+    createAssignment(event) {
+        event.preventDefault(event);
+        let dueBy;
+        try {
+            dueBy = new Date(event.target.due_by_date.value + "T" + event.target.due_by_time.value + ":00.000").toISOString();
+        } catch(e) {
+            alert("invalid date/time values");
+            return;
+        }
+        fetch(window.location.origin + "/api/assignment_definitions/", {method: "POST", body:JSON.stringify({
+            course: this.courseNumber + ":" + this.courseYear,
+            due_by: dueBy,
+            name: event.target.name.value
+        })}).then((resp) => {
+            if (resp.status !== 202) {
+                alert("error creating assignment definition. Status code is " + resp.status);
+            } else {
+                alert("assignment definition created successfully");
+            }
+            this.props.history.go(0);
+        })
+    }
+
+    selectAssignmentToDelete = {
+        mode: "radio",
+        clickToSelect: false,
+        onSelect: this.onAssignmentSelectedForDeletion
     }
 
     columns = [{
@@ -209,10 +265,147 @@ class CourseAssignmentDefs extends Component {
 
     render() {
         return <div>
-            {this.isLoaded() && this.state.elements !== null && <div><br></br><BootstrapTable hover keyField='name' data={ this.state.elements } columns={ this.columns } /></div>}
+            <Modal open={this.state.showNewAssignmentModal} center onClose={() => {this.setState({showNewAssignmentModal: false})}}>
+                <br></br>
+                <div style={{width: 455, height: 275}}><Form onSubmit={this.createAssignment}>
+                    <div class="input-group"><Form.Row>
+                        <Form.Group style={{width: 280, margin: 5}} controlId="name">
+                            <Form.Label>Name:</Form.Label>
+                            <Form.Control placeholder="Enter assignment name"/>
+                        </Form.Group>
+                    </Form.Row>
+                    <Form.Row>
+                        <Form.Group style={{width: 280, margin: 5}} controlId="due_by_date">
+                            <Form.Label>Due by date:</Form.Label>
+                            <Form.Control type="date"/>
+                        </Form.Group>
+                        <Form.Group style={{width: 280, margin: 5}} controlId="due_by_time">
+                            <Form.Label>Due by time:</Form.Label>
+                            <Form.Control type="time"/>
+                        </Form.Group>
+                    </Form.Row>
+                    <Button style={{margin: 5}} variant="primary" type="submit">Submit</Button></div>
+                </Form></div>
+            </Modal>
+            {this.isLoaded() && this.state.elements !== null && <div><br></br><BootstrapTable hover keyField="key" data={ this.state.elements } columns={ this.columns } selectRow={this.selectAssignmentToDelete}/></div>}
             {this.isLoaded() && this.state.elements === null && <div><br></br><AlertNoAssignments/></div>}
-            {this.isLoaded() && this.state.after_id > 0 && <Button variant="primary" onClick={this.previousPage}>Previous</Button>}
-            {this.isLoaded() && this.state.left_to_process && <Button variant="primary" onClick={this.previousPage}>Next</Button>}
+            <br></br>
+            {this.isLoaded() && <div class="input-group">
+                    {this.state.after_id > 0 && <Button variant="primary" onClick={this.previousPage} style={{position: "absolute", left: 0}}>Previous</Button>}
+                    {this.state.left_to_process && <Button variant="primary" onClick={this.nextPage} style={{position: "absolute", right: 0}}>Next</Button>}
+                </div>}
+            <br></br><br></br> 
+            {this.isLoaded() && <div class="input-group">
+                {this.state.elements !== null && <Button disabled={this.state.assignmentToDelete === null} style={{position: "absolute", left: 0}} onClick={this.deleteAssignment}>Delete</Button>}
+                <Button style={{position: "absolute", right: 0}} onClick={() => {this.setState({showNewAssignmentModal: true})}}>New</Button>
+            </div>}
+        </div>
+    }
+
+}
+
+class CourseAppeals extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoaded: false,
+            left_to_process: false,
+            limit: 5,
+            after_id: 0,
+            elements: null,
+        };
+        this.courseNumber = props.courseNumber;
+        this.courseYear = props.courseYear;
+        this.isLoaded = this.isLoaded.bind(this);
+        this.goToBackEnd = this.goToBackEnd.bind(this);
+        this.componentDidMount = this.componentDidMount.bind(this);
+        this.previousPage = this.previousPage.bind(this);
+        this.nextPage = this.nextPage.bind(this);
+    }
+
+    componentDidMount() {
+        this.goToBackEnd();
+    }
+
+    isLoaded() {
+        return this.state.isLoaded;
+    }
+
+    nextPage = () => {
+        this.setState({after_id: this.state.after_id + this.state.limit}, () => {
+            this.goToBackEnd()
+        })
+    }
+
+    previousPage = () => {
+        this.setState({after_id: this.state.after_id - this.state.limit}, () => {
+            this.goToBackEnd()
+        })
+    }
+
+    goToBackEnd() {
+        var url = window.location.origin + '/api/appeals/?limit='+ this.state.limit
+        if (this.state.after_id > 0) {
+          url = url + "&after_id=" + this.state.after_id
+        }
+        fetch(url, {method:'GET', 
+        headers: {'X-Submit-Course': this.courseNumber + ":" + this.courseYear}})
+        .then((response) => {
+          if (response.status !== 200){
+            alert("error fetching assignment defintions for course. Status code is " + response.status)  
+            this.props.history.push("/internal-error");
+            this.props.history.go(0);
+          }
+          if (response.headers.has("X-Elements-Left-To-Process")){
+              this.setState({left_to_process:true})
+          } else {
+              this.setState({left_to_process:false})
+          }
+          return response.json()
+        })
+        .then(data => {
+          if (data.elements !== null) {
+            for (let i = 0; i < data.elements.length; i++) {
+                let splitted = data.elements[i].assignment_instance.split(":");
+                if (splitted.length !== 4) {
+                    alert("invalid appeal key '" + data.elements[i].assignment_instance + "' returned from backend");
+                    this.props.history.push("/internal-error");
+                    this.props.history.go(0);
+                    return null;
+                }
+                data.elements[i]["assignment_def"] = splitted[2];
+                data.elements[i]["user_name"] = splitted[3];
+            } 
+          }
+          this.setState({elements:data.elements}, () => this.setState({isLoaded: true}));
+        });
+    }
+
+    columns = [{
+        dataField: "user_name",
+        text: "User"
+    },{
+        dataField: "assignment_def",
+        text: "Assignment",
+        formatter: (cell) => <a href={window.location.origin + "/assignment_definitions/" + this.courseNumber + "/" + this.courseYear + "/" + cell}>{cell}</a>
+    },{
+        dataField: "state",
+        text: "State",
+        formatter: (cell) => {
+            if (cell === 0) {
+                return <h10>Open</h10>
+            } else if (cell === 1) {
+                return <h10>Closed</h10>
+            }
+            return <h10>Invalid</h10>
+        }
+    }]
+
+    render() {
+        return <div>
+            {this.isLoaded() && this.state.elements !== null && <div><br></br><BootstrapTable hover keyField="assignment_instance" data={ this.state.elements } columns={ this.columns }/></div>}
+            {this.isLoaded() && this.state.elements === null && <div><br></br><AlertNoAppeals/></div>}
         </div>
     }
 
